9521ab7416a00874719a34d0d808f24b62141eff
zbycz <zbytovsky@gmail.com>
2015-11-18 12:36:51 +0100

    iterator add

--- src/main/java/cz/fit/dpo/cbi/arithmetic/AddOperator.java
+++ src/main/java/cz/fit/dpo/cbi/arithmetic/AddOperator.java
@@ -1,5 +1,6 @@
 package cz.fit.dpo.cbi.arithmetic;
 
+import cz.fit.dpo.cbi.arithmetic.elements.AddOperation;
 import cz.fit.dpo.cbi.arithmetic.elements.ExpressionElement;
 import java.util.Iterator;
 
@@ -20,7 +21,65 @@ public class AddOperator extends BinaryOperator {
 
     @Override
     public Iterator<ExpressionElement> getInOrderIterator() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        final ArithmeticExpression ex = this;
+
+        return new Iterator<ExpressionElement>() {
+            ArithmeticExpression expression = ex;
+            boolean iteratedSelf = false;
+            boolean iteratedFirst = false;
+            boolean iteratedSecond = false;
+
+            @Override
+            public boolean hasNext() {
+                if (!iteratedSelf && !iteratedFirst && !iteratedSecond) {
+                    return true;
+                }
+
+                return false;
+            }
+
+            @Override
+            public ExpressionElement next() {
+
+                //iterujeme nad prvním
+
+                //TODO tady závorku
+
+                if(!iteratedFirst) {
+                    boolean has = getFirstOperand().getInOrderIterator().hasNext();
+                    if(!has) { //už nemá, tak jsme ho proiterovali
+                        iteratedFirst = true;
+                    }
+                    else {
+                        return getFirstOperand().getInOrderIterator().next();
+                    }
+                }
+
+                //TODO tady závorku
+
+                // vypíšeme operátor
+                if (!iteratedSelf) {
+                    return new AddOperation();
+                }
+
+                // (
+
+                if(!iteratedSecond) {
+                    boolean has = getSecondOperand().getInOrderIterator().hasNext();
+                    if(!has) { //už nemá, tak jsme ho proiterovali
+                        iteratedFirst = true;
+                    }
+                    else {
+                        return getSecondOperand().getInOrderIterator().next();
+                    }
+                }
+
+                // )
+
+
+                throw new IllegalStateException("AddOperand cant have another next()");
+            }
+        };
     }
 
     @Override
